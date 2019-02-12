56be6552df924e3b18ef1c6adcee127b1dd5d35b
Luis Granados <granal88@gmail.com>
2019-02-12 13:00:32 -0500

    notebook prior to cleaning and finalizing ordinal econder function

--- scratch-pad.py
+++ scratch-pad.py
@@ -1,5 +1,7 @@
 """Throwaway script."""
 import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+import category_encoders as ce
 file = "/Users/luisgranados/Documents/Kaggle/House Prices/train.csv"
 
 data_in =  pd.read_csv(file)
@@ -37,5 +39,121 @@ c = b.values
 
 print(c[:])
 
+qual_rating_features = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond',
+                'HeatingQC', 'KitchenQual', 'FireplaceQu', 'GarageQual',
+                'GarageCond', 'PoolQC']
+qual_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('Ex',5), 
+        ('Gd',4), 
+        ('TA',3), 
+        ('Fa',2), 
+        ('Po',1), 
+        ('NA',0)
+    ]},
+]
 
-#print(a.head())
+# These needs own mapping
+exposure_rating_feature = ['BsmtExposure']
+exposure_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('Gd',4), 
+        ('Av',3), 
+        ('Mn',2), 
+        ('No',1), 
+        ('NA',0)
+    ]},
+]
+
+electrical_rating_feature = ['Electrical']
+electrical_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('SBrkr',5),
+        ('FuseA',4),
+        ('FuseF',3),
+        ('FuseP',2),
+        ('Mix',1)
+    ]},
+]
+
+functional_rating_feature = ['Functional']
+functional_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('Typ',7), 
+        ('Min1',6), 
+        ('Min2',5), 
+        ('Mod',4), 
+        ('Maj1',3),
+        ('Maj2',2),
+        ('Sev',1),
+        ('Sal',0)
+    ]},
+]
+
+# Finish ratings
+
+bsmt_finish_rating_feature = ['BsmtFinType1', 'BsmtFinType2']
+bsmtfin_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('GLQ',6), 
+        ('ALQ',5), 
+        ('BLQ',4), 
+        ('Rec',3), 
+        ('LwQ',2),
+        ('Unf',1),
+        ('NA',0)
+    ]},
+]
+
+grg_finish_rating_feature = ['GarageFinish']
+grgfin_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('Fin',3), 
+        ('RFn',2), 
+        ('Unf',1), 
+        ('NA',0)
+    ]},
+]
+
+paved_rating_feature = ['PavedDrive']
+paved_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('Y',2), 
+        ('P',1), 
+        ('N',0)
+    ]},
+]
+
+fence_rating_feature = ['Fence']
+fence_ordinal_map = [{
+    "col":feature,  
+    "mapping": [
+        ('GdPrv',4),
+        ('MnPrv',3),
+        ('GdWo',2), 
+        ('MnWw',1),
+        ('NA',0)
+    ]},
+]
+
+
+def mapper(data_in, features, ratings):
+    """Map ratings to numeric ranking."""
+    for feature in features:
+        encoder = ce.OrdinalEncoder(mapping = ratings,
+                                    return_df = True, cols = features)
+        df_train = encoder.fit_transform(data_in)
+    return df_train        
+                
+a = mapper(house_descr, qual_rating)
+#a['BsmtFinType1'].head()
+
+#a = mapper(house_explore, qual_rating)
+a['ExterQual'].head()
